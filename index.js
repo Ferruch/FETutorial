370ee8000193d98e959447ab5e798b5f70fd4cf2
Ferruch <88401189+Ferruch@users.noreply.github.com>
2023-03-28 11:50:56 -0600

    CSS improvement is missing
    Is missing justify text

---
+++ index.js
@@ -0,0 +1,6 @@
+let todayDate = new Date();
+let dd = todayDate.getDate();
+let mm = todayDate.getMonth();
+let yy = todayDate.getUTCFullYear();
+document.getElementById("today").innerHTML= todayDate;
+
